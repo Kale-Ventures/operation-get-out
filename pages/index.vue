7461ea7a508447b72aac33623eaeb65b2efef3b9
Eric Irish <ecirish@gmail.com>
2023-01-26 15:01:14 -0600

    update date for fall retreat

--- pages/index.vue
+++ pages/index.vue
@@ -390,7 +390,7 @@ export default {
           url: 'https://www.betterunite.com/OperationGetOut-9thannualgetoutgirlsurfpaddlejamweekend'
         },
         {
-          date: 'October 28-30, 2022',
+          date: 'October 6-8, 2023',
           title: 'OpGetOut First Responder & Military Retreat',
           location: 'Canyon Lake, TX',
           body: "A weekend retreat providing nature and water experiences with boating, wake surfing, lakeside yoga, and paddleboarding, good food and good music for front line responders and military at the beautiful Canyon Lakeview Resort, Canyon Lake, Texas. Donations and sponsorships fully fund participants' registration. To request complimentary registration or receive further information, email cindy@operationgetout.org",
